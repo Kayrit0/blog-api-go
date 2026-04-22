cda797384208bd09610952170bbdabd9581b4897
Kayrit0 <dilego95909@gmail.com>
2026-04-22 20:34:52 +0300

    updated fields

--- internal/entities/post.go
+++ internal/entities/post.go
@@ -1,7 +1,12 @@
 package entities
 
+import "time"
+
 type Post struct {
-	ID      uint   `json:"id"`
-	Title   string `json:"title"`
-	Content string `json:"content"`
+	ID        uint      `json:"id" db:"id"`
+	Title     string    `json:"title" db:"title"`
+	Content   string    `json:"content" db:"content"`
+	AuthorID  uint      `json:"author_id" db:"author_id"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
--- internal/entities/user.go
+++ internal/entities/user.go
@@ -1,8 +1,13 @@
 package entities
 
+import "time"
+
 type User struct {
-	ID       uint   `json:"id"`
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	ID        uint      `json:"id" db:"id"`
+	Username  string    `json:"username" db:"username"`
+	Email     string    `json:"email" db:"email"`
+	Password  string    `json:"-" db:"password"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	Posts     []Post    `json:"posts,omitempty" db:"-"`
 }
