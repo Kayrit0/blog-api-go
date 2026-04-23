77cef17810f4008358ea76353783a3f66263eb70
Kayrit0 <dilego95909@gmail.com>
2026-04-23 11:30:13 +0300

    add user crud operations

--- internal/handlers/users.go
+++ internal/handlers/users.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,4 +17,93 @@ func (h *Handler) GetAllUsers(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, users)
+}
+
+func (h *Handler) GetUserByID(ctx *gin.Context) {
+	id := ctx.Param("id")
+	userID, err := strconv.Atoi(id)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid user ID",
+		})
+		return
+	}
+
+	user, err := h.service.GetUserByID(uint(userID))
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, user)
+}
+
+func (h *Handler) UpdateUser(ctx *gin.Context) {
+	userID, err := strconv.ParseUint(ctx.Param("id"), 10, 32)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid user ID",
+		})
+		return
+	}
+
+	user, err := h.service.GetUserByID(uint(userID))
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"error": "user not found",
+		})
+		return
+	}
+
+	var updateData struct {
+		Username *string `json:"username" binding:"omitempty,min=3,max=50"`
+		Email    *string `json:"email" binding:"omitempty,email"`
+	}
+
+	if err := ctx.ShouldBindJSON(&updateData); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if updateData.Username != nil {
+		user.Username = *updateData.Username
+	}
+	if updateData.Email != nil {
+		user.Email = *updateData.Email
+	}
+
+	if err := h.service.UpdateUser(user); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, user)
+}
+
+func (h *Handler) DeleteUser(ctx *gin.Context) {
+	id := ctx.Param("id")
+	userID, err := strconv.Atoi(id)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid user ID",
+		})
+		return
+	}
+
+	if err := h.service.DeleteUser(uint(userID)); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "user deleted successfully",
+	})
 }
--- internal/services/users.go
+++ internal/services/users.go
@@ -5,3 +5,15 @@ import "github.com/Kayrit0/blog-api-go/internal/entities"
 func (s *Service) GetAllUsers() ([]entities.User, error) {
 	return s.repository.GetUsers(0, 50)
 }
+
+func (s *Service) GetUserByID(id uint) (*entities.User, error) {
+	return s.repository.GetUserByID(id)
+}
+
+func (s *Service) UpdateUser(updatedData *entities.User) error {
+	return s.repository.UpdateUser(updatedData)
+}
+
+func (s *Service) DeleteUser(id uint) error {
+	return s.repository.DeleteUser(id)
+}
